6d65846da6b7bf5cb49f70a1b2d88097bd312b3e
Shunji Sugaya <sugayasc33@gmail.com>
2025-06-02 10:42:08 +0900

    friday's feedback fix

--- app/javascript/controllers/google_maps_controller.js
+++ app/javascript/controllers/google_maps_controller.js
@@ -3,8 +3,98 @@ import GMaps from "gmaps"
 
 // Connects to data-controller="google-maps"
 export default class extends Controller {
-  static targets = ["search", "map", "checkboxes"]
+  static targets = ["search", "map", "checkboxes", "pinsToggle"]
   static values = { markers: Array }
+
+  connect() {
+    console.log("connected")
+    const styles = this.#snazzyStyle;
+    this.map = new GMaps({
+      el: '#map',
+      lat: 0,
+      lng: 0,
+      mapTypeControl: false,
+      fullscreenControl: false,
+      cameraControl: false
+     });
+    this.markers = JSON.parse(this.element.dataset.markers);
+    this.displayedMarkers = [];
+    this.showMarkers(this.markers);
+    // this.pins = this.map.addMarkers(this.markers);
+
+
+    this.map.addStyle({
+      styles: styles,
+      mapTypeId: 'map_style'
+    });
+    this.map.setStyle('map_style');
+  }
+
+  search(event) {
+    event.preventDefault()
+    const query = this.searchTarget.value.trim()
+    if (query.length > 0) {
+    fetch(`/places/search?query=${encodeURIComponent(query)}`)
+      .then(response => response.json())
+      .then(data => {
+        // Handle the search results here
+        this.markers = data
+        this.map.removeMarkers();
+        this.map.addMarkers(data)
+        this.reframe();
+      })
+      .catch(error => {
+        console.error('Error fetching search results:', error)
+      })
+    }
+  }
+
+  showMarkers(markers) {
+    this.map.removeMarkers();
+    this.map.addMarkers(markers);
+    this.reframe(markers);
+  }
+
+  reframe(markers = this.displayedMarkers) {
+    if (!markers) markers = [];
+    if (markers.length === 0) {
+      this.map.setZoom(2);
+    } else if (markers.length === 1) {
+      this.map.setCenter(markers[0].lat, markers[0].lng);
+      this.map.setZoom(14);
+    } else {
+      this.map.fitLatLngBounds(markers);
+    }
+  }
+
+
+  focusOn(event) {
+    const lat = parseFloat(event.currentTarget.dataset.lat)
+    const lng = parseFloat(event.currentTarget.dataset.lng)
+
+    this.map.setCenter(lat, lng)
+    this.map.setZoom(20)
+  }
+
+  filterToggle(event) {
+    // const checkedIndexes = this.checkboxesTargets.filter(cb => cb.checked).map(cb => parseInt(cb.dataset.mapIndex, 10) );
+    event.currentTarget.classList.toggle("checked")
+    const checkedIndexes = this.pinsToggleTargets.filter(pins => pins.classList.contains("checked") ).map(cb => parseInt(cb.dataset.mapIndex, 10) );
+    const filteredMarkers = this.markers.filter(marker => checkedIndexes.includes(marker.map_index));
+
+    this.showMarkers(filteredMarkers);
+  }
+
+  filterPins(event) {
+    this.pinsToggleTargets.forEach( pin => pin.classList.remove("checked") );
+    event.currentTarget.classList.add("checked");
+    const filteredPins = this.markers.filter(marker => marker.map_index ==  event.currentTarget.dataset.mapIndex);
+    this.showMarkers(filteredPins);
+  }
+
+
+
+
 
   #snazzyStyle = [{
         "featureType": "all",
@@ -175,79 +265,4 @@ export default class extends Controller {
         ]
     }];
 
-  connect() {
-    console.log("connected")
-    const styles = this.#snazzyStyle;
-    this.map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-    this.markers = JSON.parse(this.element.dataset.markers);
-
-    this.displayedMarkers = [];
-    this.showMarkers(this.markers);
-    // this.pins = this.map.addMarkers(this.markers);
-
-
-    this.map.addStyle({
-      styles: styles,
-      mapTypeId: 'map_style'
-    });
-    this.map.setStyle('map_style');
-  }
-
-  search(event) {
-    event.preventDefault()
-    const query = this.searchTarget.value.trim()
-    if (query.length > 0) {
-    fetch(`/places/search?query=${encodeURIComponent(query)}`)
-      .then(response => response.json())
-      .then(data => {
-        // Handle the search results here
-        this.markers = data
-        this.map.removeMarkers();
-        this.map.addMarkers(data)
-        this.reframe();
-      })
-      .catch(error => {
-        console.error('Error fetching search results:', error)
-      })
-    }
-  }
-
-  showMarkers(markers) {
-    this.map.removeMarkers();
-    markers.forEach(marker => {
-      if (marker.color) {
-        marker.icon = `http://maps.google.com/mapfiles/ms/icons/${marker.color}-dot.png`;
-      }
-    })
-    this.map.addMarkers(markers)
-    this.reframe(markers);
-  }
-
-  reframe(markers = this.displayedMarkers) {
-    if (!markers) markers = [];
-    if (markers.length === 0) {
-      this.map.setZoom(2);
-    } else if (markers.length === 1) {
-      this.map.setCenter(markers[0].lat, markers[0].lng);
-      this.map.setZoom(14);
-    } else {
-      this.map.fitLatLngBounds(markers);
-    }
-  }
-
-
-  focusOn(event) {
-    const lat = parseFloat(event.currentTarget.dataset.lat)
-    const lng = parseFloat(event.currentTarget.dataset.lng)
-
-    this.map.setCenter(lat, lng)
-    this.map.setZoom(20)
-  }
-
-  filterToggle() {
-    const checkedIndexes = this.checkboxesTargets.filter(cb => cb.checked).map(cb => parseInt(cb.dataset.mapIndex, 10) );
-    const filteredMarkers = this.markers.filter(marker => checkedIndexes.includes(marker.map_index));
-
-    this.showMarkers(filteredMarkers);
-  }
 }
