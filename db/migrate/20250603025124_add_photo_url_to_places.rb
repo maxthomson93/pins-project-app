0aeda958457e4f20d9facc14dfd1af540d2131fe
saulo leal <sauloleal62@gmail.com>
2025-06-03 12:41:05 +0900

    merging problem fixed

--- db/migrate/20250603025124_add_photo_url_to_places.rb
+++ db/migrate/20250603025124_add_photo_url_to_places.rb
@@ -1,5 +1,9 @@
-class AddPhotoUrlToPlaces < ActiveRecord::Migration[7.1]
+# db/migrate/20250603025124_add_photo_url_to_places.rb
+
+class AddPhotoUrlToPlaces < ActiveRecord::Migration[6.1]
   def change
-    add_column :places, :photo_url, :string
+    unless column_exists?(:places, :photo_url)
+      add_column :places, :photo_url, :string
+    end
   end
 end
